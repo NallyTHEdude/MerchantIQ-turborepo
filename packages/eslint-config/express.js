967c3e3b3453a965d03999ec6d287a32be51f0a3
Nawaz <rifaqatnawaz25@gmail.com>
2026-08-27 16:46:28 +0530

    chore: added express config

---
+++ packages/eslint-config/express.js
@@ -0,0 +1,30 @@
+import globals from "globals";
+import { config as baseConfig } from "./base.js";
+
+/**
+ * A shared ESLint configuration for Express/server packages.
+ *
+ * @type {import("eslint").Linter.Config[]}
+ */
+export const expressConfig = [
+  ...baseConfig,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+];
+
+export default expressConfig;
